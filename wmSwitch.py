9c7c094aa5d6beceb2fa2f0789ca9b7b5255f878
AlexanderAdelhardt <77829049+AlexanderAdelhardt@users.noreply.github.com>
2021-01-22 09:39:56 +0100

    Create wmSwitch.py

---
+++ wmSwitch.py
@@ -0,0 +1,5 @@
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.compute.models import DiskCreateOption
